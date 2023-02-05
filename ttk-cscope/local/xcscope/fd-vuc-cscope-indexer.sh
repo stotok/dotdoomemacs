08537bafd9e43bbf04da2f7c8076791ae4416636
Totok Sulistiomono <s.totok@gmail.com>
2023-02-06 06:55:02 +0800

    Remove parcecdb from the cscope indexer script

--- ttk-cscope/local/xcscope/fd-vuc-cscope-indexer.sh
+++ ttk-cscope/local/xcscope/fd-vuc-cscope-indexer.sh
@@ -78,25 +78,4 @@ ${FIND} '\.([ch](xx|pp)|[ch]|cc|hh)$' \
 echo "generating cscope database: ${DATABASE_FILE} ..."
 ${CSCOPE} -b -c -i ${LIST_FILE} -f ${DATABASE_FILE}
 #
-CDBPYSCRIPTLOC="VUC_APP/tools/parsecdb"
-CDBNAME="compile_commands.json"
-CDBENVVAR="cdbenv.var"
-APPROOTDIR="TVIP/Vehicle_Micro"
-FBLROOTDIR="VUC_APP/User_Config/Boot_SBU/SBU"
-# generating compile_commands.json for APPROOTDIR
-pushd "${APPROOTDIR}" > /dev/null
-python2 "parsecdb.py" --in "${CDBENVVAR}" --out "${CDBNAME}"
-popd > /dev/null
-### generating compile_commands.json for FBLROOTDIR
-### disable first
-# pushd "${FBLROOTDIR}" > /dev/null
-# python2 "parsecdb.py" --in "${CDBENVVAR}" --out "${CDBNAME}"
-# popd > /dev/null
-### combining compile_commands.json
-CDBIN1="${APPROOTDIR}/${CDBNAME}"
-CDBIN2="${FBLROOTDIR}/${CDBNAME}"
-CDBOUT="${CDBNAME}"             # at current directory
-python2 VUC_APP/tools/parsecdb/combinecdb.py --in1 "${CDBIN1}" --in2 "${CDBIN2}" --out "${CDBOUT}"
-echo "generated ${CDBOUT}"
-#
 echo "done."
